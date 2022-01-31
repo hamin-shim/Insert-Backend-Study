1d61f9aaf5f8af48db7c56c6a6ff02b044bd7a2d
alrbs10 <89731330+alrbs10@users.noreply.github.com>
2022-01-31 15:46:29 +0900

    ~14 done (hamin)

--- hamin/Dwitter/server/router/tweets.js
+++ hamin/Dwitter/server/router/tweets.js
@@ -1,12 +1,18 @@
 import express from "express";
 import "express-async-errors";
+import { body } from "express-validator";
 import * as tweetController from "../controller/tweet.js";
-
+import { validate } from "../middleware/validator.js";
 const router = express.Router();
+const validateTweet = [
+  body("text").trim().isLength({ min: 3 }).withMessage("text more than 3 char"),
+  body("text").isEmail().withMessage("empty"),
+  validate,
+];
 router.get("/", tweetController.getTweets);
 router.get("/:id", tweetController.getTweet);
-router.post("/", tweetController.createTweet);
+router.post("/", validateTweet, tweetController.createTweet);
 
-router.put("/:id", tweetController.updateTweet);
+router.put("/:id", validateTweet, tweetController.updateTweet);
 router.delete("/:id", tweetController.deleteTweet);
 export default router;
