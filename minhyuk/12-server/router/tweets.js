35e404d736d89d65971704c9988b573f4564b610
alsgur7859 <alsgur7859@naver.com>
2022-02-06 18:16:21 +0900

    mvc

--- minhyuk/12-server/router/tweets.js
+++ minhyuk/12-server/router/tweets.js
@@ -1,76 +1,38 @@
 import express from 'express';
 import 'express-async-errors';
+import * as tweetController from '../controller/tweet.js';
 
-let tweets = [
-    {
-        id: '1',
-        text: '화이팅!',
-        createdAt: Date.now().toString(),
-        name: 'Bob',
-        username: 'bob',
-        url: 'https://werwer.com',
-    },
-    {
-        id: '2',
-        text: '화이팅22!',
-        createdAt: Date.now().toString(),
-        name: 'Ellie',
-        username: 'ellie',
-    }
-];
+// let tweets = [
+//     {
+//         id: '1',
+//         text: '화이팅!',
+//         createdAt: Date.now().toString(),
+//         name: 'Bob',
+//         username: 'bob',
+//         url: 'https://werwer.com',
+//     },
+//     {
+//         id: '2',
+//         text: '화이팅22!',
+//         createdAt: Date.now().toString(),
+//         name: 'Ellie',
+//         username: 'ellie',
+//     }
+// ];
 
 const router = express.Router();
 
 //GEt /tweets
 //GET /tweets?username=:username
-router.get('/', (req, res, next) => {
-    const username = req.query.username;
-    const data = username 
-    ? tweets.filter(t => t.username === username)
-    : tweets;
-    res.status(200).json(data);
-});
+router.get('/', tweetController.getTweets);//getTweets()아니고 getTweets!! 호출하면 안됨.
 //GET /tweets/:id
-router.get('/:id', (req, res, next) => {
-    const id = req.params.id;
-    const tweet = tweets.find(t => t.id === id);
-    if(tweet){
-        res.status(200).json(tweet);
-    } else{
-        res.status(404).json({message: 'Tweet id('+id+') not found'});
-    }
-})
+router.get('/:id', tweetController.getTweet)
 //POST /tweets
-router.post('/', (req, res, next) => {
-    const {text, name, username} = req.body;
-    const tweet = {
-        id: Date.now().toString(),
-        text,
-        createdAt: new Date(),
-        name,
-        username,
-    };
-    tweets = [tweet, ...tweets];
-    res.status(201).json(tweet);
-})
+router.post('/', tweetController.createTweet)
 
 //PUT /tweets/:id
-router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    const text = req.body.text;
-    const tweet = tweets.find((tweet) => tweet.id === id);
-    if(tweet) {
-        tweet.text = text;
-        res.status(200).json(tweet);
-    } else {
-        res.status(404).json({message: 'Tweet id('+id+') not found'});
-    }
-})
+router.put('/:id', tweetController.updateTweet)
 //DELETE /tweets/:id
-router.delete('/:id', (req, res, next) => {
-    const id = req.params.id;
-    tweets = tweets.filter(t => t.id !== id);
-    res.sendStatus(204);
-})
+router.delete('/:id', tweetController.deleteTweet)
 
 export default router;
